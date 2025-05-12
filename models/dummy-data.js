f4bc4286af123e845c4f998dc47704be9c761156
Batuhan <91565731+BizonTR@users.noreply.github.com>
2025-05-12 21:43:31 +0300

    dummy data games added

--- models/dummy-data.js
+++ models/dummy-data.js
@@ -1,6 +1,6 @@
 const Game = require("../models/game");
 const Users = require("../models/users");
-const userCategory = require("../models/userCategory");
+const userCategory = require("../models/usercategory");
 const bcrypt = require('bcrypt');
 
 async function populate() {
@@ -32,33 +32,28 @@ async function populate() {
         await Game.create({
             title: "Witcher 3",
             description: "RPG",
-            isActive: true,
             url: "https://cdn1.epicgames.com/offer/14ee004dadc142faaaece5a6270fb628/EGS_TheWitcher3WildHuntCompleteEdition_CDPROJEKTRED_S1_2560x1440-82eb5cf8f725e329d3194920c0c0b64f",
-            userId: 1,
         });
+
+        // 10 tane daha oyun ekleyelim
+        const games = [
+            { title: "Shadow Of The Tomb Raider", description: "Adventure", url: "https://salaodejogos.net/wp-content/uploads/2018/09/ShadowofTombRaider.jpg" },
+            { title: "Need For Speed: Hot Pursuit Remastered", description: "Race Game", url: "https://media.contentapi.ea.com/content/dam/eacom/need-for-speed-hot-pursuit-remastered/images/2020/09/nfs-youtube-cover-image-16x9-l.jpg.adapt.crop16x9.320w.jpg" },
+            { title: "Age Of Empires III: Definitive Edition", description: "Strategy", url: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2477660/capsule_616x353.jpg?t=1691100434" },
+            { title: "Red Dead Redemption 2", description: "Western Adventure", url: "https://cdn1.epicgames.com/b30b6d1b4dfd4dcc93b5490be5e094e5/offer/RDR2476298253_Epic_Games_Wishlist_RDR2_2560x1440_V01-2560x1440-2a9ebe1f7ee202102555be202d5632ec.jpg" },
+            { title: "Elden Ring", description: "Fantasy RPG", url: "https://image.api.playstation.com/vulcan/img/rnd/202111/0506/hcFeWRVGHYK72uOw6Mn6f4Ms.jpg" },
+            { title: "Mount And Blade II: Bannerlord", description: "Medieval", url: "" },
+            { title: "Ready Or Not", description: "Shooter, Tactical", url: "" },
+            { title: "Stardew Valley", description: "Farm Game", url: "" },
+            { title: "Left 4 Dead 2", description: "Shooter, Tactical", url: "" },
+            { title: "Phasmophobia", description: "Horror", url: "" },
+            { title: "Euro Truck Simulator 2", description: "Truck Driving", url: "" }
+        ];
+
+        for (const game of games) {
+            await Game.create(game);
+        }
     }
 }
-
-// userCategory verilerini güncelleyelim
-const userData = async () => {
-    const count = await userCategory.count();
-    if (count === 0) {
-        await userCategory.bulkCreate([
-            { categoryname: "Süper Admin" }, // ID=1
-            { categoryname: "Moderatör" },  // ID=2
-            { categoryname: "Normal Kullanıcı" } // ID=3
-        ]);
-
-        // İlk kullanıcı olarak süper admin oluştur
-        const hashedPassword = await bcrypt.hash("admin", 10);
-        await Users.create({
-            name: "Admin",
-            surname: "User",
-            email: "admin@example.com",
-            password: hashedPassword,
-            usercategoryId: 1 // Süper Admin
-        });
-    }
-};
 
 module.exports = populate;
